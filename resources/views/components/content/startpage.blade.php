2c870e95f4dc39dba2fdc2316a01a11b6761f45d
JustSteve <stefanvdrehle@gmail.com>
2024-03-23 14:09:17 +0100

    changed startpage design welcome text

--- resources/views/components/content/startpage.blade.php
+++ resources/views/components/content/startpage.blade.php
@@ -2,20 +2,12 @@
   <div class="h-screen bg-center bg-auto bg-schuetzenhaus">
     <div class="flex flex-row justify-around max-w-6xl gap-32 pt-40 mx-auto">
       <x-application-logo class="w-[25%] fill-current text-gray-500" />
-      <div class="bg-custom-green-900 bg-opacity-70 from-transparent p-3 max-w-[50%]">
-        <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos blanditiis eum minus.
-          Aspernatur dignissimos
-          assumenda quae, hic quas numquam neque nihil maiores voluptatum, doloremque voluptatem rerum dolorem
-          dolor,
-          magni fuga. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum soluta commodi blanditiis
-          saepe
-          itaque temporibus numquam facere ad nobis beatae cumque magni dolorum consequatur atque, sapiente
-          ipsa odit
-          vero tempora? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, quas cum esse
-          dolorem
-          exercitationem architecto! Incidunt, fugiat distinctio quis recusandae numquam possimus dolore.
-          Velit facilis
-          accusamus magni perspiciatis quis rem!</p>
+      <div class="bg-custom-green-900 bg-opacity-70 from-transparent p-3 max-w-[50%] flex">
+        <p class="text-center text-white text-[54px] self-center drop-shadow-lg font-serif">Herzlich willkommen auf der
+          Webseite
+          des
+          Schützenvereins Kommern!
+        </p>
       </div>
     </div>
   </div>
