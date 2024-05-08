d6e4fd3c33e3622e27dafa3cb44e10e17acc6218
JustSteve <stefanvdrehle@gmail.com>
2024-05-08 16:13:07 +0200

    wording image modal

--- resources/views/components/modals/imageview.blade.php
+++ resources/views/components/modals/imageview.blade.php
@@ -4,6 +4,6 @@
   </div>
 
   <x-slot:action>
-    <a :href="inputData.imageUrl" class="px-4 py-2 font-extrabold uppercase duration-150 ease-in-out bg-primary text-accent-50 hover:bg-accent" download>Datei runterladen</a>
+    <a :href="inputData.imageUrl" class="px-4 py-2 font-extrabold uppercase duration-150 ease-in-out bg-primary text-accent-50 hover:bg-accent" download>Datei herunterladen</a>
   </x-slot>
 </x-modal>
