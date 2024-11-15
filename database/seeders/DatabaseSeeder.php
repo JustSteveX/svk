84eae4f0f9c9c8ef3f8e1e08f5da091e37a5d385
JustSteveX <develop@steve-designs.de>
2024-11-15 16:35:28 +0100

    fixed seeder problems

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $files_arr = scandir(dirname(__FILE__)); //store filenames into $files_array
+        /*$files_arr = scandir(dirname(__FILE__)); //store filenames into $files_array
         $blacklist_arr = ['AlbumSeeder.php'];
         foreach ($files_arr as $key => $file) {
             echo $file.$blacklist_arr[0];
             if (in_array($file, $blacklist_arr) || $file !== 'DatabaseSeeder.php' && $file[0] !== '.') {
                 $this->call(explode('.', $file)[0]);
             }
-        }
+        }*/
+        $this->call([
+          RoleSeeder::class,
+          UserSeeder::class,
+          AlbumSeeder::class
+        ]);
     }
 }
